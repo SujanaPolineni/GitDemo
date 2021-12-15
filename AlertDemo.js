ae130a58d5ec07bc61893513c98ee465fce4ad36
SujanaPolineni <sujanapqa@gmail.com>
2021-12-15 15:31:04 +0530

    deleted maximize code

--- AlertDemo.js
+++ AlertDemo.js
@@ -2,11 +2,11 @@ describe('Protractor Alert demo',function(){
 
     it('Open Non-Angular js website Alerts',function(){
         browser.waitForAngularEnabled(false);
-        browser.driver.window().maxi
+
         browser.get('https://rahulshettyacademy.com/AutomationPractice/');
         element(by.id("confirmbtn")).click();
         browser.switchTo().alert().dismiss().then(function(){
-            browser.sleep(5000);
+            browser.sleep(2000);
         })
     })
 
